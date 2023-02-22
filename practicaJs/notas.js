9d65dcee04e2690a427d8e017d1b8b842c7570e8
CamilaArangoA <milaarango2000@gmail.com>
2023-02-22 07:51:37 -0500

    first commit

---
+++ practicaJs/notas.js
@@ -0,0 +1,9 @@
+var Nota1 = parseFloat(prompt("digite nota 1"))
+var Nota1 = parseFloat(prompt("digite nota 3"))
+var Nota1 = 4
+var Nota1 = 0
+
+promedio =(Nota1+Nota2+Nota3)/3
+console.log("el promedio de nota es"+Promedio)
+
+
